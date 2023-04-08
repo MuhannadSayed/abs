8c8bd2e90139507c49ebabe283aed3d29fcd127b
muhannadsayedabdullah <symohannad@gmail.com>
2023-04-08 08:18:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import { absences } from "./data.js";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      absences,
+    };
+  },
+  methods() {
+    console.log("hi new");
+  },
+  mounted() {
+    console.log("hi new");
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
